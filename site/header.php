6a91a9035230963ce1f8423e12e0692c8a9c2974
Denis <35287846+DEN4IK2115@users.noreply.github.com>
2022-02-02 17:47:14 +0300

    Test Branch

--- site/header.php
+++ site/header.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <body>
@@ -8,13 +9,13 @@
 						<span>Путевод</span>
 					</a>
 				</div>
-<!--				
+<!--
 				<div id="about">
 					<a href="" title="Узнать больше о достопримечательностях">Достопримечательности</a>
 					<a href="" title="Где поесть">Рестораны</a>
 				</div>
--->				
-<!--				
+-->
+<!--
                 <div id="reg_auth">
 					<a href="" title="Войти в кабинет пользователя">
 						<div id="btn">
@@ -37,14 +38,14 @@
 					<a href="/restaurant">Поесть</a>
 					<a href="/relax">Отдохнуть</a>
 				</div>
-<!--				
+<!--
 				<div id="search">
 					<span>Поиск</span>
 					<i class="fas fa-search"></i>
 				</div>
--->				
+-->
 			</nav>
-	
+
 	<!-- jQuery -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script>
@@ -54,7 +55,7 @@
 			else
 				$('mobileMenu').show();
 		});
-		
+
 		$(document).scroll (function () {
 			if($(document).scrollTop() > $('header').height () + 10)
 				$('nav').addClass('fixed');
@@ -64,3 +65,105 @@
 	</script>
 </body>
 </html>
+
+
+<!--
+				<div id="search">
+					<span>Поиск</span>
+					<i class="fas fa-search"></i>
+				</div>
+-->
+<!--
+				<div id="search">
+					<span>Поиск</span>
+					<i class="fas fa-search"></i>
+				</div>
+-->	
+<!--
+				<div id="search">
+					<span>Поиск</span>
+					<i class="fas fa-search"></i>
+				</div>
+-->	
+=======
+<!DOCTYPE html>
+<html>
+<body>
+			<header>
+				<div id="logo">
+					<a href="http://travelperm.000webhostapp.com" title="На главную">
+						<img src="img/Logo.jpg" title="Путевод" alt="Путевод">
+						<span>Путевод</span>
+					</a>
+				</div>
+<!--
+				<div id="about">
+					<a href="" title="Узнать больше о достопримечательностях">Достопримечательности</a>
+					<a href="" title="Где поесть">Рестораны</a>
+				</div>
+-->
+<!--
+                <div id="reg_auth">
+					<a href="" title="Войти в кабинет пользователя">
+						<div id="btn">
+							Войти
+						</div>
+					</a>
+					<a href="" title="Зарегистрироваться на сайте">
+						<div id="btn">
+							Регистрация
+						</div>
+					</a>
+				</div>
+-->
+			</header>
+			<nav>
+				<div id="menuShow"><i class="fa fa-bars" aria-hidden="true"></i></div>
+				<div id="hideMenu">
+					<a href="/lions">Достопримечательности</a>
+					<a href="/place">Интересные места</a>
+					<a href="/restaurant">Поесть</a>
+					<a href="/relax">Отдохнуть</a>
+				</div>
+<!--
+				<div id="search">
+					<span>Поиск</span>
+					<i class="fas fa-search"></i>
+				</div>
+-->
+			</nav>
+
+	<!-- jQuery -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script>
+		$('#menuShow').click (function () {
+			if ($('mobileMenu').is(':visible'))
+				$('mobileMenu').hide();
+			else
+				$('mobileMenu').show();
+		});
+
+		$(document).scroll (function () {
+			if($(document).scrollTop() > $('header').height () + 10)
+				$('nav').addClass('fixed');
+			else
+				$('nav').removeClass('fixed');
+		});
+	</script>
+</body>
+</html>
+
+
+<!--
+				<div id="search">
+					<span>Поиск</span>
+					<i class="fas fa-search"></i>
+				</div>
+-->
+<!--
+				<div id="search">
+					<span>Поиск</span>
+					<i class="fas fa-search"></i>
+				</div>
+-->	
+>>>>>>> b8763b403bf23dacb2f7311dcec5d578f533c12f
